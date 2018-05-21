b27ebfcc2faaaec396aa7419f82b9077fb97a33c
Anton Sardak <anton.sardak@ya.ru>
2018-05-21 18:02:31 +0300

    Add ability to delete named range

--- defined_names.go
+++ defined_names.go
@@ -12,6 +12,20 @@ func (f *File) AddDefinedName(sheet, begin, end, name string) {
 	endCol, endRow := getCellColRow(end)
 	r.DefinedNames.DefinedName = append(r.DefinedNames.DefinedName, xlsxDefinedName{
 		Name: name,
-		Data: fmt.Sprintf("%s!$%s$%s:$%s$%s", sheet, beginCol, beginRow, endCol, endRow),
+		Data: fmt.Sprintf("'%s'!$%s$%s:$%s$%s", sheet, beginCol, beginRow, endCol, endRow),
 	})
+}
+
+func (f *File) DeleteDefinedName(name string) {
+	r := f.workbookReader()
+	if r.DefinedNames == nil {
+		r.DefinedNames = &xlsxDefinedNames{}
+	}
+
+	for i := 0; i < len(r.DefinedNames.DefinedName); i++ {
+		if r.DefinedNames.DefinedName[i].Name == name {
+			r.DefinedNames.DefinedName = append(r.DefinedNames.DefinedName[:i], r.DefinedNames.DefinedName[i+1:]...)
+			return
+		}
+	}
 }
